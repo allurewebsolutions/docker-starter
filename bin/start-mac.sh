e2a7ba4f508b18a80e8d4d9cb2d4b1dc32b2dcf8
Mike Doubintchik <info@allurewebsolutions.com>
2017-09-27 14:03:17 -0400

    Updated start script to be more robust

--- bin/start-mac.sh
+++ bin/start-mac.sh
@@ -1,2 +1,28 @@
-docker-sync-stack start
-sh bin/traefik-helper.sh up -d
+#!/usr/bin/env bash
+PARAMS=""
+if [ -z "$*" ]; then
+    PARAMS="--build"
+fi
+
+# Build containers
+echo
+echo "Building containers..."
+docker-compose pull
+if [ -e "docker-custom.yml" ]; then
+	docker-compose -f docker-compose.yml -f docker-custom.yml up ${PARAMS} $@
+else
+    docker-sync start
+    docker-compose up ${PARAMS} $@
+    sh bin/traefik-helper.sh up $@
+fi
+
+echo
+echo "Done"
+
+read -r -p "Do you want to install a fresh copy of WordPress? " response
+
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    sh bin/install-wordpress.sh
+else
+    echo "Project Initiated."
+fi
