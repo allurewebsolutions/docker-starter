3de219aaac0f7e66be3f97410568ae2cabc4ddd5
Mike Doubintchik <info@allurewebsolutions.com>
2017-09-27 14:03:02 -0400

    Updated install wordpress script to be more robust

--- bin/install-wordpress.sh
+++ bin/install-wordpress.sh
@@ -6,5 +6,26 @@ then
 else
 	echo "WordPress config file not found. Installing..."
 	docker-compose exec --user 82 php wp core download
-	docker-compose exec --user 82 php wp core config --dbhost=mariadb --dbname=wordpress --dbuser=root --dbpass=password
+
+    echo "Setting up config file..."
+
+	read -r -p "Database name (must match what you set in docker-compose.yml or press enter to use default): " database
+
+	database=${database:-wordpress}
+
+	docker-compose exec --user 82 php wp core config --dbhost=mariadb --dbname=$database --dbuser=root --dbpass=password
+
+	read -r -p "Is your database already setup? " response
+
+	if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+	    echo "Exiting."
+	else
+	    read -r -p "Development URL (must match what you set in your docker-compose.yml; ie. test.docker.localhost): " url
+	    read -r -p "Site Name: " name
+	    read -r -p "Username: " usernam
+	    read -r -p "Password: " password
+	    read -r -p "Email: " email
+
+	     docker-compose exec --user 82 php wp core install --url=$url --title="$name" --admin_name=$username --admin_password=$password --admin_email=$email
+	fi
 fi
