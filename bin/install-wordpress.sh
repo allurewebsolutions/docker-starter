7ce30c4a7839350282ed498458ae4a9af405c367
Mike Doubintchik <info@allurewebsolutions.com>
2017-10-13 15:26:22 -0400

    Improve start-mac bin script

--- bin/install-wordpress.sh
+++ bin/install-wordpress.sh
@@ -2,7 +2,29 @@
 
 if [ -f "./www/wp-config.php" ];
 then
-	echo "WordPress config file found."
+	read -r -p "WordPress config file found. Do you want to install or reinstall the database? " reinstall
+
+	if [[ "$reinstall" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+	    docker-compose exec --user 82 php wp core download
+
+        echo "Setting up config file..."
+
+        read -r -p "Database name (must match what you set in docker-compose.yml or press enter to use default): " database
+
+        database=${database:-wordpress}
+
+        docker-compose exec --user 82 php wp core config --dbhost=mariadb --dbname=$database --dbuser=root --dbpass=password
+
+        read -r -p "Development URL (must match what you set in your docker-compose.yml; ie. test.docker.localhost): " url
+        read -r -p "Site Name: " name
+        read -r -p "Username: " username
+        read -r -p "Password: " password
+        read -r -p "Email: " email
+
+        docker-compose exec --user 82 php wp core install --url=$url --title="$name" --admin_name=$username --admin_password=$password --admin_email=$email
+	else
+	    echo "Exiting."
+	fi
 else
 	echo "WordPress config file not found. Installing..."
 	docker-compose exec --user 82 php wp core download
@@ -22,10 +44,10 @@ else
 	else
 	    read -r -p "Development URL (must match what you set in your docker-compose.yml; ie. test.docker.localhost): " url
 	    read -r -p "Site Name: " name
-	    read -r -p "Username: " usernam
+	    read -r -p "Username: " username
 	    read -r -p "Password: " password
 	    read -r -p "Email: " email
 
-	     docker-compose exec --user 82 php wp core install --url=$url --title="$name" --admin_name=$username --admin_password=$password --admin_email=$email
+	    docker-compose exec --user 82 php wp core install --url=$url --title="$name" --admin_name=$username --admin_password=$password --admin_email=$email
 	fi
 fi
